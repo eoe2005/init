f17ff2220cdad29d664ba7d12e96b8d3f33db72c
吃掉大象的蚂蚁 <eoe2005@gmail.com>
2025-01-13 16:23:13 +0000

    fx

---
+++ getall.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+cd ..
+git config --global user.name "吃掉大象的蚂蚁"
+git config --global user.email 'eoe2005@gmail.com'
+
+git clone git@github.com:eoe2005/gphplib.git
+git clone git@github.com:eoe2005/gphpapp.git
+git clone git@github.com:eoe2005/GeminiProChat.git
+git clone git@github.com:eoe2005/CFWK.git
+git clone git@github.com:eoe2005/platformio_wch583m.git
+git clone git@github.com:eoe2005/platformio_wch583m_sdk.git
+git clone git@github.com:eoe2005/isr-blog-nextjs-wordpress.git
+git clone git@github.com:eoe2005/pserver.git
+git clone git@github.com:eoe2005/g.git
+git clone git@github.com:eoe2005/goapp.git
+git clone git@github.com:eoe2005/goutils.git
+git clone git@github.com:eoe2005/learn.git
+git clone git@github.com:eoe2005/github-host.git
+git clone git@github.com:eoe2005/composer-genSwooleApi.git
+git clone git@github.com:eoe2005/flutter-app-gay.git
+git clone git@github.com:eoe2005/flutter-yuhui.git
---
+++ github.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+mkdir g-npm gcalf ghf-go gserct eoe2005
+
+cd eoe2005
+git clone git@github.com:eoe2005/init.git
+cd init
+./getall.sh
+cd ../../
+
+cd g-npm
+git clone git@github.com:g-npm/init.git
+cd init
+./getall.sh
+cd ../../
+
+
+cd gcalf
+git clone git@github.com:gcalf/init.git
+cd init
+./getall.sh
+cd ../../
+
+cd gserct
+git clone git@github.com:gserct/init.git
+cd init
+./getall.sh
+cd ../../
+
+cd ghf-go
+git clone git@github.com:ghf-go/init.git
+cd init
+./getall.sh
+cd ../../
