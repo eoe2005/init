1d5632022ad1d86a44253d36385fdcaca3223f67
吃掉大象的蚂蚁 <eoe2005@gmail.com>
2025-01-14 16:03:01 +0800

    更新全局脚本

--- github.sh
+++ github.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-mkdir g-npm gcalf ghf-go gserct eoe2005
+mkdir g-npm gcalf ghf-go gserct eoe2005 ghf-php
 
 cd eoe2005
 git clone git@github.com:eoe2005/init.git
 cd init
 ./getall.sh
 cd ../../
+
+cd ghf-php
+git clone git@github.com:ghf-php/init.git
+cd init
+./getall.sh
+cd ../../
+
 
 cd g-npm
 git clone git@github.com:g-npm/init.git
