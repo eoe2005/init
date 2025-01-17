46e2a3e533aef3afe9ff6d5845018f0ab2563562
吃掉大象的蚂蚁 <eoe2005@gmail.com>
2025-01-17 12:27:46 +0800

    清理仓库

--- getall.sh
+++ getall.sh
@@ -6,13 +6,10 @@ git config --global user.email 'eoe2005@gmail.com'
 
 git clone git@github.com:eoe2005/gphplib.git
 git clone git@github.com:eoe2005/gphpapp.git
-git clone git@github.com:eoe2005/CFWK.git
-git clone git@github.com:eoe2005/isr-blog-nextjs-wordpress.git
 git clone git@github.com:eoe2005/g.git
 git clone git@github.com:eoe2005/goapp.git
 git clone git@github.com:eoe2005/goutils.git
 git clone git@github.com:eoe2005/learn.git
-git clone git@github.com:eoe2005/github-host.git
 git clone git@github.com:eoe2005/composer-genSwooleApi.git
 git clone git@github.com:eoe2005/flutter-app-gay.git
 git clone git@github.com:eoe2005/flutter-yuhui.git
